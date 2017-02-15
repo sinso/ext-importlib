01da445a9bf2ad05ef318e5527dffd62f8421f0f
Simon Gadient <simon.gadient@swisscom.com>
2017-02-15 18:35:12 +0100

    [IMP] Logging on file sync

--- Classes/Service/SysFileSyncService.php
+++ Classes/Service/SysFileSyncService.php
@@ -1,12 +1,19 @@
 <?php
 namespace Sinso\Importlib\Service;
 
+use TYPO3\CMS\Core\Log\LogLevel;
+
 class SysFileSyncService {
 
     /**
      * @var \Sinso\Importlib\Service\SimpleSyncService
      */
     protected $simpleSyncService;
+
+    /**
+     * @var string
+     */
+    protected $importName;
 
     /**
      * @var string
@@ -24,6 +31,11 @@ class SysFileSyncService {
     protected $resourceUid;
 
     /**
+     * @var \TYPO3\CMS\Core\Log\Logger
+     */
+    protected $logger;
+
+    /**
      * SysFileSyncService constructor.
      * @param string $importName
      * @param string $tableName
@@ -31,8 +43,10 @@ class SysFileSyncService {
      */
     public function __construct($importName, $tableName, $resourcePath) {
         $this->simpleSyncService = new SimpleSyncService($importName, 'sys_file_reference');
+        $this->importName = $importName;
         $this->tableName = $tableName;
         $this->resourcePath = $resourcePath;
+        $this->logger = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance('TYPO3\CMS\Core\Log\LogManager')->getLogger(__CLASS__);
     }
 
     public function initializeResource($recordUid, $resourceUrl, $syncStrategy = SimpleSyncService::SYNC_PREFER_SOURCE) {
@@ -71,37 +85,56 @@ class SysFileSyncService {
      * @return bool|int
      */
     public function syncPhysicalResource($url, $syncStrategy = SimpleSyncService::SYNC_PREFER_SOURCE) {
+        $uid = 0;
         $localName = \TYPO3\CMS\Core\Utility\PathUtility::basename($url);
         $fullLocalPath = $this->resourcePath . $localName;
-        $targetResourceExists = file_exists($fullLocalPath);
-        $targetResourceHasChanged = FALSE; //TODO: detect changes necessary for imported files?
-        $sourceResourceHasChanged = TRUE; //TODO: detect changes (ETAG, CURLOPT_NOBODY+CURLOPT_FILETIME)
+        $syncResource = TRUE;
 
-        $syncResource = FALSE;
-        if ($targetResourceExists && !$sourceResourceHasChanged && !$targetResourceHasChanged) {
+        if (file_exists($fullLocalPath)) {
             $syncResource = FALSE;
+            /* TODO: Detect changes and sync resources according the logic below
+            $targetResourceHasChanged = FALSE; //TODO: detect changes necessary for imported files?
+            $sourceResourceHasChanged = TRUE; //TODO: detect changes (ETAG, CURLOPT_NOBODY+CURLOPT_FILETIME)
+            if ($sourceResourceHasChanged) { // Change on source
+                if ($targetResourceHasChanged) { // Change on target -> conflict
+                    if ($syncStrategy === SimpleSyncService::SYNC_PREFER_TARGET) { // No change
+                        $syncResource = FALSE;
+                    }
+                }
+            }*/
         }
-
-        if ($syncStrategy === SimpleSyncService::SYNC_PREFER_TARGET && $targetResourceExists && $targetResourceHasChanged) {
-            $syncResource = FALSE;
-        }
-        //TODO: what if file was deleted intentionally and import should not create it again?
 
         if ($syncResource) {
-            $fileData = \TYPO3\CMS\Core\Utility\GeneralUtility::getURL($url);
-            if (! $fileData) {
-                return FALSE;
+            if ($fileData = \TYPO3\CMS\Core\Utility\GeneralUtility::getURL($url)) {
+                $this->logger->log(LogLevel::INFO, 'Sync Resource: Loaded file from URL', $this->getLogData());
+                if (\TYPO3\CMS\Core\Utility\GeneralUtility::writeFile($fullLocalPath, $fileData)) {
+                    $this->logger->log(LogLevel::INFO, 'Sync Resource: Wrote file to local', $this->getLogData());
+                } else {
+                    $this->logger->log(LogLevel::ERROR, 'Sync Resource: Write file to local FAILED', $this->getLogData());
+                }
+            } else {
+                $this->logger->log(LogLevel::ERROR, 'Sync Resource: Load file from URL FAILED', $this->getLogData());
             }
+        }
 
-            if (!\TYPO3\CMS\Core\Utility\GeneralUtility::writeFile($fullLocalPath, $fileData)) {
-                return FALSE;
-            }
+        if ($fileObject = \TYPO3\CMS\Core\Resource\ResourceFactory::getInstance()->retrieveFileOrFolderObject($fullLocalPath)) {
+            $uid = $fileObject->getUid();
+            $this->logger->log(LogLevel::INFO, 'Sync Resource: Read file from local', $this->getLogData());
+        } else  {
+            $this->logger->log(LogLevel::ERROR, 'Sync Resource: Read file from local FAILED', $this->getLogData());
         }
-        try {
-            $fileObject = \TYPO3\CMS\Core\Resource\ResourceFactory::getInstance()->retrieveFileOrFolderObject($fullLocalPath);
-            return $fileObject->getUid();
-        } catch (\Exception $e) {
-            return FALSE;
+        return $uid;
+    }
+
+    private function getBaseLogData() {
+        return array('importName' => $this->importName, 'tableName' => $this->tableName);
+    }
+
+    private function getLogData() {
+        $additionalLogData = $this->getBaseLogData();
+        if (! empty($this->resourceUid)) {
+            $additionalLogData['resourceUid'] = $this->resourceUid;
         }
+        return $additionalLogData;
     }
 }
